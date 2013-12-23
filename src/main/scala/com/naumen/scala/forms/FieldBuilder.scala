7c1549d7099dd100dd28cb43d9f8228f4861891d
ygrabovskiy <ygrabovskiy@naumen.ru>
2013-12-23 14:13:42 +0600

    minor improvements

--- src/main/scala/com/naumen/scala/forms/FieldBuilder.scala
+++ src/main/scala/com/naumen/scala/forms/FieldBuilder.scala
@@ -41,16 +41,17 @@ case class FormDescription(fields: Map[String, FieldDescription] = Map(), attrs:
 
 case class FormDescriptionBuilder[+T: Manifest](fields: Map[String, FieldDescription] = Map(), attrs: Map[String, Any] = Map()) extends FieldNameGetter {
 
-  //    import play.api.data.Forms._
+    type FieldFoo[F] = FieldDescriptionBuilder[F] => FieldDescriptionBuilder[F]
+    type FormField[F] = FieldFoo[F] => FormDescriptionBuilder[T]
 
-  def emptyStringBuilder = FieldDescriptionBuilder[String](Map())
+    def emptyStringBuilder = FieldDescriptionBuilder[String](Map())
 
   def addFieldDescription(name: String, fb: FieldDescription) = copy(fields = fields + (name -> fb))
 
-  def field[F: Manifest](fieldFoo: T => F)(foo: FieldDescriptionBuilder[F] => FieldDescriptionBuilder[F]) =
+  def field[F: Manifest](fieldFoo: T => F)(foo: FieldFoo[F]) =
     fieldWithCustomName($[T](fieldFoo))(foo)
 
-  def fieldOpt[F: Manifest](fieldFoo: T => Option[F])(foo: FieldDescriptionBuilder[F] => FieldDescriptionBuilder[F]) =
+  def fieldOpt[F: Manifest](fieldFoo: T => Option[F])(foo: FieldFoo[F]) =
     fieldWithCustomName($[T](fieldFoo))(foo)
 
 
@@ -58,38 +59,35 @@ case class FormDescriptionBuilder[+T: Manifest](fields: Map[String, FieldDescrip
     foo(this).asInstanceOf[FormDescriptionBuilder[T]]
   }
 
-  def fieldWithCustomName[F: Manifest](fieldName: String)(foo: FieldDescriptionBuilder[F] => FieldDescriptionBuilder[F]) = {
+  def fieldWithCustomName[F: Manifest](fieldName: String)(foo: FieldFoo[F]) = {
 
     val initialBuilder = FieldDescriptionBuilder[F](Map()).addProperty(FieldDescription.FieldType, implicitly[Manifest[F]].runtimeClass)
 
     addFieldDescription(fieldName, foo(initialBuilder).build)
   }
 
-  def boolean(fieldFoo: T => Boolean) = fieldBase[Boolean](fieldFoo)(_.required) _
+  def boolean(fieldFoo: T => Boolean): FormField[Boolean] = fieldBase[Boolean](fieldFoo)(_.required)
 
-  def string(fieldFoo: T => String) =
-    fieldBase[String](fieldFoo)(_.required) _
+  def string(fieldFoo: T => String): FormField[String] =
+    fieldBase[String](fieldFoo)(_.required)
 
-  def stringOpt(fieldFoo: T => Option[String]) =
-    fieldBase[String](fieldFoo)(identity) _
+  def stringOpt(fieldFoo: T => Option[String]): FormField[String] =
+    fieldBase[String](fieldFoo)(identity)
 
-  def string(name: String) =
-    fieldWithCustomName[String](name) _
+  def int(fieldFoo: T => Int): FormField[Int] = fieldBase[Int](fieldFoo)(_.required)
 
-  def int(fieldFoo: T => Int) = fieldBase[Int](fieldFoo)(_.required) _
-
-  def intOpt(fieldFoo: T => Option[Int]) = fieldBase[Int](fieldFoo)(identity) _
+  def intOpt(fieldFoo: T => Option[Int]): FormField[Int] = fieldBase[Int](fieldFoo)(identity)
     
-  def bigDecimal(fieldFoo: T => BigDecimal) = fieldBase[BigDecimal](fieldFoo)(_.required) _
+  def bigDecimal(fieldFoo: T => BigDecimal): FormField[BigDecimal] = fieldBase[BigDecimal](fieldFoo)(_.required)
 
-  def bigDecimalOpt(fieldFoo: T => Option[BigDecimal]) = fieldBase[BigDecimal](fieldFoo)(identity) _
+  def bigDecimalOpt(fieldFoo: T => Option[BigDecimal]): FormField[BigDecimal] = fieldBase[BigDecimal](fieldFoo)(identity)
 
   def fieldBase[F: Manifest](fieldFoo: T => Any)
-                            (foo: FieldDescriptionBuilder[F] => FieldDescriptionBuilder[F])
-                            (foo2: FieldDescriptionBuilder[F] => FieldDescriptionBuilder[F])
+                            (foo: FieldFoo[F])
+                            (foo2: FieldFoo[F])
   = fieldWithCustomName[F]($[T](fieldFoo))(foo andThen foo2)
 
-  def seq[A: Manifest](fieldFoo: T => Seq[A])(foo: FieldDescriptionBuilder[Seq[A]] => FieldDescriptionBuilder[Seq[A]])   = {
+  def seq[A: Manifest](fieldFoo: T => Seq[A])(foo: FieldFoo[Seq[A]]) = {
     val elementType = implicitly[Manifest[A]].runtimeClass
 
     fieldBase[Seq[A]](fieldFoo)(_.addProperty(FieldDescription.ListElementType, elementType))(foo)
@@ -97,8 +95,8 @@ case class FormDescriptionBuilder[+T: Manifest](fields: Map[String, FieldDescrip
 
   import FieldCustomizers._
   val defaultDateFormat = new SimpleDateFormat("dd.MM.yyyy").toPattern
-  def dateOpt(fieldFoo: T => Option[Date]) = fieldBase[Date](fieldFoo)(_.format(defaultDateFormat))  _
-  def date(fieldFoo: T => Date) = fieldBase[Date](fieldFoo)(_.required.format(defaultDateFormat))  _
+  def dateOpt(fieldFoo: T => Option[Date]): FormField[Date] = fieldBase[Date](fieldFoo)(_.format(defaultDateFormat))
+  def date(fieldFoo: T => Date): FormField[Date] = fieldBase[Date](fieldFoo)(_.required.format(defaultDateFormat))
 
   def name(n: String) = copy(attrs = attrs + ("name" -> n))
 
